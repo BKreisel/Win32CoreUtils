532e2745843d2bed0302b1f406cbedaace36864e
Brandon Kreisel <BKreisel@users.noreply.github.com>
2020-04-28 09:32:02 -0400

    Naive implementation of cat.

---
+++ src/cat/config.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Config {
+    pub files: Vec<PathBuf>
+}
+
+impl Config {
+    pub fn new(args: env::Args) -> Result<Config, ()> {
+        if args.len() == 1 {
+            Config::help();
+            return Err(());
+        }
+
+        let files: Vec::<PathBuf> = args.skip(1)
+            .filter(|arg| !arg.starts_with('-'))
+            .map(|arg| PathBuf::from(arg))
+            .collect();
+       
+        Ok(Config {
+            files
+        })
+    }
+
+    pub fn help() {
+        let usage = "Usage: cat [OPTION] ... [FILE] ...
+Concatenate FILE(s) to standard output.
+";
+        println!("{}", usage);
+    }
+}
+
